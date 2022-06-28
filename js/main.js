f5054a7675d41b00211fc44a40b81672e619a2cb
Thiago Vieira <thiagovieira.dg@gmail.com>
2022-06-28 09:56:18 -0300

    criado repositorio - site basico - responsividade pendente

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
-// swiper slider
+// swiper slider home atualizado
 
-var swiper = new Swiper(".bg-slider-thumbs", {
-    loop: true,
-    spaceBetween: 0,
-    slidesPerView: 0,
-  });
-  var swiper2 = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 0,
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+var swiper = new Swiper(".mySwiper", {
+  
+  autoplay: {
+    delay: 8000,
+  },
+
+  loop: true,
+
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+    
 
 /* efeitos na navegação e rolagem do site */
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // efeitos da barra de rolagem de navegação
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
@@ -34,34 +43,34 @@ const closeBtn = document.querySelector(".nav-close-btn");
 const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
-navigation.classList.add("active");
+  navigation.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
   navigation.classList.remove("active");
-  });
+});
 
-  // fim slider home
+// fim slider home
 
-  // inicio slider produtos
+// inicio slider produtos
 
-  var swiper = new Swiper('.swiper', {
-    autoplay: {
-      delay: 5000,
-    },
+var swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 8000,
+  },
 
-    loop: true,
+  loop: true,
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      disableOnInteraction: false,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    disableOnInteraction: false,
+  },
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  // fim slider produtos
+// fim slider produtos
